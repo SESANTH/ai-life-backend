967797a5d114e48d341ef0047df910a9c02abc3e
Sesanth <sesanth22@gmail.com>
2026-04-13 15:21:04 +0530

    fix cors credentials issue

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# 🔥 MOVE THIS HERE (TOP)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=False,  # IMPORTANT CHANGE
     allow_methods=["*"],
     allow_headers=["*"],
 )
