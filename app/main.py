9753cac67065befd8fc33f6017e4196ff2d6eeb6
Sesanth <sesanth22@gmail.com>
2026-04-13 14:56:39 +0530

    fix cors order

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-from app.db.database import engine, Base
-from app.db import models
-from app.routers import task_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-#create tables
-Base.metadata.create_all(bind=engine)
-
-app.include_router(task_router.router)
-
-@app.get("/")
-def root():
-    return {"message": "AI Life Assistant Backend Running 🚀"}
-
-from app.routers import chat_router
-
-app.include_router(chat_router.router)
-
+# 🔥 MOVE THIS HERE (TOP)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,3 +11,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+from app.db.database import engine, Base
+from app.db import models
+from app.routers import task_router, chat_router
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(task_router.router)
+app.include_router(chat_router.router)
+
+@app.get("/")
+def root():
+    return {"message": "AI Life Assistant Backend Running 🚀"}
